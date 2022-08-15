21e2866f438700ab634347499088c6edb500a640
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-15 10:15:36 +0200

    Project done with configureState file added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { Store } from 'redux';
+import { store } from './redux/configureState';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App></App>
+      <App />
     </Provider>
   </React.StrictMode>,
 );
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,19 +1,19 @@
 const BOOK_ADDED = 'BOOK_ADDED';
 const BOOK_REMOVED = 'BOOK_REMOVED';
 
-export const doAddBook = (book) => ({type :BOOK_ADDED, playload : book});
-export const doRemoveBook = (id) => ({type :BOOK_REMOVED, id});
+export const doAddBook = (book) => ({ type: BOOK_ADDED, playload: book });
+export const doRemoveBook = (id) => ({ type: BOOK_REMOVED, id });
 
 const initialState = [];
-const booksReducer = (state = initialState, action ) => {
-    switch (action.type) {
-        case BOOK_ADDED:
-            return [...state, action.playload];
-        case BOOK_REMOVED:
-            return state.filter((book) = book.id !== action.id);
-        default:
-            return state;
-    }
-}
+const booksReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BOOK_ADDED:
+      return [...state, action.playload];
+    case BOOK_REMOVED:
+      return state.filter((book) = book.id !== action.id);
+    default:
+      return state;
+  }
+};
 
 export default booksReducer;
