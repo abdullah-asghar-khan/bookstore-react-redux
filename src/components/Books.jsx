03fbb0ecf8be6ef6508958c0b88b612aed6d8b51
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-16 09:48:16 +0200

    implementing remove book functionality

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import AddBookForm from './AddBookForm';
 
-const Books = () => (
-  <div>
-    <Book title="Rich Dad" author="Robert Kiyosaki" />
-    <AddBookForm />
-  </div>
-);
+const Books = () => {
+  const books = useSelector((state) => state.books);
+
+  return (
+    <div>
+      <ul>
+        {books.map((book) => (
+          <Book
+            title={book.title}
+            key={book.id}
+            author={book.author}
+            id={book.id}
+          />
+        ))}
+      </ul>
+      <AddBookForm />
+    </div>
+  );
+};
 
 export default Books;
