db3af9645186971c8fdec1dd647e20395bb75d10
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-16 09:58:30 +0200

    Linters errors fixing

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -5,23 +5,23 @@ export const doAddBook = (book) => ({ type: BOOK_ADDED, playload: book });
 export const doRemoveBook = (id) => ({ type: BOOK_REMOVED, id });
 
 const initialState = [
-    {
-      title: 'The Wolf and the Watchman',
-      author: 'Niklas Natt',
-      id: '1',
-    },
-    {
-      title: 'The Glided Wolves',
-      author: 'Oshani Chokshi',
-      id: '2',
-    },
-    {
-      title: 'The House of Broken Angels',
-      author: 'Luis Alberto',
-      id: '3',
-    },
-  
-  ];
+  {
+    title: 'The Wolf and the Watchman',
+    author: 'Niklas Natt',
+    id: '1',
+  },
+  {
+    title: 'The Glided Wolves',
+    author: 'Oshani Chokshi',
+    id: '2',
+  },
+  {
+    title: 'The House of Broken Angels',
+    author: 'Luis Alberto',
+    id: '3',
+  },
+
+];
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case BOOK_ADDED:
